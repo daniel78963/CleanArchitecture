3f2845451e4bdee6e0b1dccca32fda0b4416eace
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-05-18 22:05:12 -0500

    Domain.Interface

---
+++ CleanArchitecture/Domain.Interface/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync();
+
+        IAsyncRepository<T> AsyncRepository<T>() where T : BaseEntity;
+    }
+}
